463b52059ffc3f869b206dfbf6506fb31723c87e
xudenden <56295612@qq.com>
2019-12-11 13:33:19 +0800

    2019-12-10 update

--- HelloWorld/src/main.cpp
+++ HelloWorld/src/main.cpp
@@ -1,96 +1,79 @@
 #include <Arduino.h>
-#include <algorithm>
+#include <Servo.h>
+#include <Thread.h>
+#include <ThreadController.h>
 #include <vector>
+// ThreadController that will controll all threads
+ThreadController control = ThreadController();
+
+//My Thread
+Thread servo0_thread = Thread();
+//His Thread
+Thread servo1_thread = Thread();
+//ThreadController, that will be added to controll
+ThreadController group_of_threads = ThreadController();
+
+#define NUMBER_OF_SERVOS 2
 using namespace std;
-// - Pins are all defined here.
-vector<int> TriggerPins = {
-    PB8,
-    PB6,
-    PB4,
-    PG15,
-    // PG13,
-    // PG11,
-    // PG9,
-    // PD6,
-    // PD4,
-    // PD2,
-};
-vector<int> EchoPins = {
-    PB9,
-    PB7,
-    PB5,
+vector<int> servo_signal_pins_pool = {
     PB3,
-    // PG14,
-    // PG12,
-    // PG10,
-    // PD7,
-    // PD5,
-    // PD3,
+    PB4,
+    PB6,
+    PB7,
+    PB8,
+    PB9,
 };
-vector<int> VCC = {
-    PB10,
-    PE14,
-    PE12,
-    PE10,
-    PE8,
-    PG1,
-};
-vector<int> GND = {
-    PB11,
-    PE15,
-    PE13,
-    PE11,
-    PE9,
-    PE7,
-};
-unsigned long Time_Echo_us = 0;
-unsigned long Len_mm = 0;
+vector<int> servo_signal_pins;
+vector<Servo> servos;
+vector<int> pos; // variable to store the servo position
+void servo0_motion();
+void servo1_motion();
+
 void setup()
 {
-    // Initialize trigger and echo pins as output and input
-    for (unsigned int i = 0; i < TriggerPins.size(); i++) {
-        pinMode(TriggerPins[i], OUTPUT);
-    }
-    for (unsigned int i = 0; i < EchoPins.size(); i++) {
-        pinMode(EchoPins[i], INPUT);
-    }
-
-    // Setting VCC and GND as output and initialize their value
-    for (unsigned int i = 0; i < VCC.size(); i++) {
-        pinMode(VCC[i], OUTPUT);
-        digitalWrite(VCC[i], HIGH);
-    }
-    for (unsigned int i = 0; i < GND.size(); i++) {
-        pinMode(GND[i], OUTPUT);
-        digitalWrite(GND[i], LOW);
-    }
-
-    // Begin receiving signals from the serial port
+    // Allocate pins according to the number of servos to be used
+    servos.resize(NUMBER_OF_SERVOS);
     Serial.begin(115200);
+    for (int i = 0; i < NUMBER_OF_SERVOS; i++) {
+        // Getting pins from pins pool
+        servo_signal_pins.push_back(servo_signal_pins_pool.back());
+        servo_signal_pins_pool.pop_back();
+        // Attaching pins
+        pinMode(servo_signal_pins[i], OUTPUT);
+        servos[i].attach(servo_signal_pins[i]);
+        Serial.print("Servo No.");
+        Serial.print(i, DEC);
+        Serial.print("Attached to pin");
+        Serial.println(servo_signal_pins[i], DEC);
+        // Initializing servo positions
+        pos.push_back(0);
+    }
+    // Configure threads
+    servo0_thread.onRun(servo0_motion);
+    servo0_thread.setInterval(10);
+    servo1_thread.onRun(servo1_motion);
+    servo1_thread.setInterval(1800);
+    control.add(&servo1_thread);
+    control.add(&servo0_thread);
 }
-
+void servo1_motion()
+{
+    pos[1] %= 170;
+    pos[1] += 10;
+    servos[1].write(pos[1]);
+}
+void servo0_motion()
+{
+    static bool di_up = true;
+    if (pos[0] == 150) {
+        di_up = false;
+    } else if (pos[0] == 30) {
+        di_up = true;
+    }
+    pos[0] += di_up ? 1 : -1;
+    servos[0].write(pos[0]);
+}
 void loop()
 {
-    // ? Assume their's equal number of trigger pins ans echo pins
-    // ? Might not be a good design
-    // ? Currently using the number of trigger pins to begin the for loop
-    for (unsigned int i = 0; i < TriggerPins.size(); i++) {
-        digitalWrite(TriggerPins[i], HIGH);        //begin to send a high pulse, then US-025/US-026 begin to measure the distance
-        delayMicroseconds(10);                     //set this high pulse width as 10us (>10us)
-        digitalWrite(TriggerPins[i], LOW);         //end this high pulse
-        Time_Echo_us = pulseIn(EchoPins[i], HIGH); //calculate the pulse width at EchoPin,
-        if (Time_Echo_us > 1 && Time_Echo_us < 66000) {
-            Len_mm = (Time_Echo_us * 34 / 100) / 2; //calculate the distance by pulse width, Len_mm = (Time_Echo_us * 0.34mm/us) / 2 (mm)
-            // Serial.print("Echo pulse time is: ");
-            // Serial.print(Time_Echo_us, DEC);
-            // Serial.println("us");
-            Serial.print(i, DEC);
-            Serial.print("st Present Distance is: "); //output result to Serial monitor
-            Serial.print(Len_mm, DEC);
-            Serial.println("mm");
-        } else {
-            Serial.println("Too Far or Too Close!");
-        }
-        delay(100); // Set the time interval between pulses to 100ms
-    }
+    control.run();
 }
